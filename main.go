8372d9a98db2c1f471662d7f1aa410ef6ee1da7a
Thomas Sileo <thomas.sileo@gmail.com>
2016-12-19 22:59:56 +0100

    Better ouput and cache dir

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -12,9 +13,39 @@ import (
 	"strings"
 	"time"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/tsileo/blobstash/pkg/client/docstore"
 	"gopkg.in/yaml.v2"
 )
+
+var cache string
+
+var errCacheFileExist = errors.New("cache file already exist")
+
+func printErr(msg string, err error) {
+	fmt.Printf("%s: %v", msg, err)
+	os.Exit(1)
+}
+
+func cacheDir() (string, error) {
+	// Get the home directory
+	d, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+
+	// The cache dir will be stored at `~/var/blobs-cli`
+	p := filepath.Join(d, "var", "blobs-cli")
+
+	// Create the directory if needed
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		if err := os.MkdirAll(p, 0700); err != nil {
+			return "", err
+		}
+	}
+
+	return p, nil
+}
 
 var Usage = func() {
 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
@@ -79,7 +110,7 @@ func fmtBlobs(blobs []*Blob, shortHashLen int) {
 			return
 		}
 		index[shortHash] = blob.ID
-		buf.WriteString(fmt.Sprintf("%s  %s  %s  %s\n", time.Unix(updated, 0).Format("2006-01-02T15:04:05"), shortHash, t, blob.Title))
+		buf.WriteString(fmt.Sprintf("%s  %s  %s  %s\n", time.Unix(updated, 0).Format("2006-01-02  15:04"), shortHash, t, blob.Title))
 	}
 	data, err := json.Marshal(index)
 	if err != nil {
@@ -91,13 +122,27 @@ func fmtBlobs(blobs []*Blob, shortHashLen int) {
 	fmt.Printf("%s", buf.String())
 }
 
-func toEditor(id string, data []byte) ([]byte, error) {
-	// FIXME(tsileo): check if it already exist, and handle the restoration like swp vim
-	fpath := filepath.Join(os.TempDir(), fmt.Sprintf("blob_%s", id))
-	if err := ioutil.WriteFile(fpath, data, 0644); err != nil {
+type editedBlob struct {
+	data []byte
+	path string
+}
+
+func (eb *editedBlob) remove() error {
+	return os.Remove(eb.path)
+}
+
+func toEditor(id string, data []byte) (*editedBlob, error) {
+	fpath := filepath.Join(cache, fmt.Sprintf("blob_%s", id))
+
+	// Ensure we won't overwrite a file that need to be recovered
+	if _, err := os.Stat(fpath); !os.IsNotExist(err) {
+		return nil, errCacheFileExist
+	}
+
+	if err := ioutil.WriteFile(fpath, data, 0640); err != nil {
 		return nil, fmt.Errorf("failed to create temp file: %s", err)
 	}
-	defer os.Remove(fpath)
+
 	// Spawn $EDITOR and wait for its exit
 	// FIXME(tsileo): use $EDITOR
 	cmd := exec.Command("vim", fpath)
@@ -111,18 +156,24 @@ func toEditor(id string, data []byte) ([]byte, error) {
 	if err := cmd.Wait(); err != nil {
 		return nil, fmt.Errorf("failed to edit: %s", err)
 	}
-	// Read the file back
+
+	// The editor has been closed, read the file back
 	data2, err := ioutil.ReadFile(fpath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open temp file: %s", err)
 	}
-	return data2, nil
+
+	// The called will be responsible for removing the file
+	return &editedBlob{
+		path: fpath,
+		data: data2,
+	}, nil
 }
 
 func dataToBlob(data []byte) (*Blob, error) {
 	parts := bytes.Split(data, []byte("---\n"))
 	if len(parts) != 3 {
-		return nil, fmt.Errorf("bad input")
+		return nil, fmt.Errorf("missing YAML header")
 	}
 	header := &BlobYAMLHeader{}
 	if err := yaml.Unmarshal(parts[1], &header); err != nil {
@@ -135,10 +186,9 @@ func dataToBlob(data []byte) (*Blob, error) {
 		Meta:     header.Meta,
 		Content:  string(parts[2]),
 	}, nil
-
 }
 
-type SearchQuery struct {
+type searchQuery struct {
 	Fields      []string `json:"fields"`
 	QueryString string   `json:"qs"`
 }
@@ -155,6 +205,16 @@ func main() {
 		Usage()
 		os.Exit(2)
 	}
+
+	var err error
+	// Init cache
+	cache, err = cacheDir()
+	if err != nil {
+		fmt.Printf("failed to init cache dir: %v", err)
+		os.Exit(1)
+	}
+
+	// Try to load the previous shortID cache index if present
 	index := map[string]string{}
 	orig, err := ioutil.ReadFile(filepath.Join(os.TempDir(), fmt.Sprintf("blobs_refs_index.json")))
 	if err != nil && !os.IsNotExist(err) {
@@ -172,6 +232,7 @@ func main() {
 		}
 		return id
 	}
+
 	switch flag.Arg(0) {
 	case "recent", "r":
 		iter, err := col.Iter(nil, nil)
@@ -183,7 +244,9 @@ func main() {
 		if err := iter.Err(); err != nil {
 			panic(err)
 		}
+		// FIXME(tsileo): an option to display full hash
 		fmtBlobs(resp.Blobs, 3)
+
 	case "search", "s":
 		if flag.Arg(1) == "" {
 			fmt.Printf("no query")
@@ -191,7 +254,7 @@ func main() {
 		}
 		iter, err := col.Iter(&docstore.Query{
 			StoredQuery: "blobs-search",
-			StoredQueryArgs: &SearchQuery{
+			StoredQueryArgs: &searchQuery{
 				Fields:      []string{"title", "content"},
 				QueryString: flag.Arg(1),
 			},
@@ -226,29 +289,50 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		updatedBlob, err := dataToBlob(data)
+		updatedBlob, err := dataToBlob(data.data)
 		if err != nil {
 			panic(err)
 		}
+
+		// Ensure the blob has been modified
 		if updatedBlob.Title != blob.Title || updatedBlob.Content != blob.Content {
 			if err := col.UpdateID(blob.ID, updatedBlob); err != nil {
 				panic(err)
 			}
 		}
+
+		// Now we can safely delete the temp file
+		if err := data.remove(); err != nil {
+			panic(err)
+		}
 		// fmt.Printf("blob=%+v", updatedBlob)
+
 	case "new", "n":
 		out := []byte("---\nupdated: false\ntitle: Untitled\n---\n")
 		data, err := toEditor(fmt.Sprintf("%d", time.Now().Unix()), out)
 		if err != nil {
 			panic(err)
 		}
-		updatedBlob, err := dataToBlob(data)
+		updatedBlob, err := dataToBlob(data.data)
 		if err != nil {
 			panic(err)
 		}
-		if _, err := col.Insert(updatedBlob, nil); err != nil {
+
+		// TODO(tsileo): a warning on empty notes?
+		// if updatedBlob.Content == "" {}
+
+		_id, err := col.Insert(updatedBlob, nil)
+		if err != nil {
 			panic(err)
 		}
+
+		// Now we can safely delete the temp file
+		if err := data.remove(); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Blob %s created", _id.String())
+
 	case "download", "dl":
 		blob := &Blob2{}
 		if err := col.GetID(expand(flag.Arg(1)), &blob); err != nil {
@@ -261,7 +345,11 @@ func main() {
 		if err := ds.DownloadAttachment(parts[1], blob.Title); err != nil {
 			panic(err)
 		}
+
+		fmt.Printf("Blob downloaded at %s", blob.Title)
+
 	case "upload", "u":
+		// FIXME(tsileo): iter over all `flag.Arg(x)` and support tagging them?
 		ref, err := ds.UploadAttachment(flag.Arg(1))
 		if err != nil {
 			panic(err)
@@ -271,27 +359,63 @@ func main() {
 			Type:  "file",
 			Ref:   "#blobstash/json:" + ref,
 		}
-		if _, err := col.Insert(blob, nil); err != nil {
+		_id, err := col.Insert(blob, nil)
+		if err != nil {
+			// FIXME(tsileo): display the ref of the attachment so a note can be created without re-uploading it
 			panic(err)
 		}
+		fmt.Printf("Blob %s created", _id.String())
+
 	case "convert":
 		orig, err := ioutil.ReadFile(flag.Arg(1))
 		if err != nil {
 			panic(err)
 		}
-		out := []byte("---\nupdated: false\ntitle: Untitled\n---\n")
+		title := filepath.Base(flag.Arg(1))
+		out := []byte(fmt.Sprintf("---\narchived: false\ntitle: '%s'\n---\n", title))
 		out = append(out, orig...)
 		data, err := toEditor(fmt.Sprintf("%d", time.Now().Unix()), out)
 		if err != nil {
 			panic(err)
 		}
-		updatedBlob, err := dataToBlob(data)
+
+		updatedBlob, err := dataToBlob(data.data)
 		if err != nil {
 			panic(err)
 		}
-		if _, err := col.Insert(updatedBlob, nil); err != nil {
+
+		_id, err := col.Insert(updatedBlob, nil)
+		if err != nil {
 			panic(err)
 		}
+
+		// Now we can safely delete the temp file
+		if err := data.remove(); err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Blob %s created", _id.String())
+
+	case "restore":
+		d, err := os.Open(cache)
+		if err != nil {
+			printErr("failed to open cache dir", err)
+		}
+		fis, err := d.Readdir(-1)
+		if err != nil {
+			printErr("failed to read cache dir", err)
+		}
+		for _, fi := range fis {
+			if strings.HasPrefix(fi.Name(), "blob_") {
+
+				// buf.WriteString(fmt.Sprintf("%s  %s  %s  %s\n", time.Unix(updated, 0).Format("2006-01-02  15:04"), shortHash, t, blob.Title))
+
+			}
+		}
+		if err := d.Close(); err != nil {
+			printErr("failed to close cache dir", err)
+		}
+
 	default:
 		Usage()
 	}
